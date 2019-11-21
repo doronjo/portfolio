c50ce3ad782ebb218723b0361038bac9e2ee2e55
doronjo <josepe.doron@gmail.com>
2019-11-21 08:29:54 +0200

    init proj

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nav',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent { 
+
+    constructor(){
+
+    }
+
+}
